3ae7d5633ef1d144910f7361f77a7db0c2f66c24
Marc LOPES <marc_lopes2004@yahoo.fr>
2023-01-19 12:15:32 +0900

    dev: add UsersView and UserView

---
+++ src/views/UserView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import UserDetails from "../components/UserDetails.vue";
+
+const props = defineProps({
+  id: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <main>
+    <UserDetails :id="props.id" />
+  </main>
+</template>
---
+++ src/views/UsersView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import UsersGrid from "../components/UsersGrid.vue";
+import sourceData from "../data.json";
+import { ref } from "vue";
+
+const searchQuery = ref("");
+const users = sourceData.users;
+const columns = ["name", "bio"];
+</script>
+
+<template>
+  <div class="actions">
+    <form>
+      <input name="query" v-model="searchQuery" placeholder="🔍 Search user" />
+    </form>
+  </div>
+  <UsersGrid :users="users" :columns="columns" :searchQuery="searchQuery" />
+</template>
+
+<style>
+.actions {
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 8px;
+}
+
+form img {
+  background-color: #42b983;
+  width: 5%;
+}
+</style>
