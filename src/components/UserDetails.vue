8f93edefbb5443150dc9fa4bacb00820df26ae20
Marc LOPES <marc_lopes2004@yahoo.fr>
2023-01-19 12:14:57 +0900

    dev: add User Details component

---
+++ src/components/UserDetails.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { RouterLink } from "vue-router";
+import sourceData from "../data.json";
+
+const props = defineProps({
+  id: {
+    type: Number,
+    required: true,
+  },
+});
+
+const users = sourceData.users;
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Back to users</RouterLink>
+  </nav>
+  <br />
+  <div v-if="users[props.id - 1]" class="card">
+    <img
+      :src="`/images/${users[props.id - 1].picture}`"
+      :alt="users[props.id - 1].name"
+    />
+    <h1>{{ users[props.id - 1].name }}</h1>
+    <hr />
+    <br />
+    <p>{{ users[props.id - 1].bio }}</p>
+  </div>
+  <div v-else>
+    <p>User not found</p>
+  </div>
+</template>
